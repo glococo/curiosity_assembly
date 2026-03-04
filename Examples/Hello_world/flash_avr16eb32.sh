6baedddbc567f823a85a868889b64cfbf818f25c
glococo <glococo@gmail.com>
2026-03-04 20:50:21 +0000

    Refactoring

--- Examples/Hello_world/flash_avr16eb32.sh
+++
@@ -1,8 +0,0 @@
-avr-gcc -mmcu=avr16eb32 -Wl,--gc-sections -Wa,-gstabs -Wall -o main.elf main.S
-avr-objcopy -O ihex main.elf main.hex
-#avr-objdump -s -m avr6 -d -h main.elf
-avr-objdump -s -m avr6 -D main.hex
-avrdude -v -c pkobn_updi -p avr16eb32 -U flash:w:main.hex
-rm main.elf main.hex
-
-# DUMP: avrdude -v -c pkobn_updi -p avr16eb32 -U flash:r:board.hex:i
---
+++ curiosity.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Check if MCU and BOARD are provided as command-line arguments
+if [ -z "$1" ] || [ -z "$2" ] || [ -z "$3" ]; then
+    echo "Usage: $0 <MCU> <BOARD_FILE> <PROGRAM_FILE>"
+    echo "Example: $0 avr16eb32 Boards/AVR16EB32_CNANO.S Examples/Hello_world/main.S"
+    exit 1
+fi
+
+MCU=$1
+BOARD=$2
+PROGRAM=$3
+
+# Compile and link
+avr-gcc -mmcu=$MCU -Wl,--gc-sections -Wa,-gstabs -Wall -o main.elf -include Boards/$BOARD.S $PROGRAM
+
+# Create HEX file
+avr-objcopy -O ihex main.elf main.hex
+
+# Disassemble HEX file for inspection
+avr-objdump -s -m avr6 -D main.hex
+
+# Flash to device using avrdude
+avrdude -v -c pkobn_updi -p $MCU -U flash:w:main.hex
+
+# Cleanup temporary build files
+rm main.elf main.hex
+
+# DUMP: avrdude -v -c pkobn_updi -p $MCU -U flash:r:board.hex:i
